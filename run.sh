188bda7198bbd909e88d60315ed2937856ac5805
Mike Farrow <contact@mikefarrow.co.uk>
2016-04-15 19:29:45 +0100

    fixing working dir

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 COMPOSER_CACHE_DIR="$WERCKER_CACHE_DIR/weyforth/composer"
 COMPOSER_CACHE_BIN_FILE=composer.phar
 COMPOSER_CACHE_BIN="$COMPOSER_CACHE_DIR/$COMPOSER_CACHE_BIN_FILE"
+COMPOSER_USER=${WERCKER_COMPOSER_USER:-$WERCKER_COMPOSER_ENV_USER}
+COMPOSER_USER=${COMPOSER_USER:-root}
+COMPOSER_PATH="$PWD"
 
 mkdir -p "$COMPOSER_CACHE_DIR"
 
@@ -12,10 +15,6 @@ mkdir -p "$COMPOSER_CACHE_DIR"
 cp "$COMPOSER_CACHE_BIN" /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
 
-COMPOSER_USER=${WERCKER_COMPOSER_USER:-$WERCKER_COMPOSER_ENV_USER}
-
-COMPOSER_USER=${COMPOSER_USER:-root}
-
 [ "$COMPOSER_USER" != "root" ] && chsh -s /bin/sh "$COMPOSER_USER"
 
-su -c "composer install" - "$COMPOSER_USER"
+su -c "composer install --working-dir=\"$COMPOSER_PATH\"" - "$COMPOSER_USER"
